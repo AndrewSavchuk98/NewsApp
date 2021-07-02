4783d78350945c91a46cf4e08abacc97f534ad32
Krotko_Mori <andrej.petrenko.110@gmail.com>
2021-07-02 14:27:06 +0300

    Add view binding and finish navigation

--- app/src/main/java/com/example/savchuk/newsappmvvm/ui/NewsActivity.kt
+++ app/src/main/java/com/example/savchuk/newsappmvvm/ui/NewsActivity.kt
@@ -2,12 +2,25 @@ package com.example.savchuk.newsappmvvm.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.savchuk.newsappmvvm.R
+import com.example.savchuk.newsappmvvm.databinding.ActivityNewsBinding
 
 class NewsActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_news)
+        val binding = ActivityNewsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val newsNavHostFragment = supportFragmentManager.findFragmentById(R.id.newsNavHostFragment) as NavHostFragment
+        binding.bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
 
 
     }
